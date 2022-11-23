5578effcfb731d71c9cf262ff48ae85597ca022b
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-11-23 17:54:29 +0530

    implement sticky header

--- components/Header.js
+++ components/Header.js
@@ -10,46 +10,48 @@ function Header() {
   return (
     <>
       {/* // Left */}
-      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
-            alt="logo"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
-            alt="logo"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-
-        <div className="relative">
-          <div className="absolute top-2 left-2">
-            <MagnifyingGlassIcon className="h-5" />
+      <div className="shadow-sm border-b sticky top-0 z-30 bg-white">
+        <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+          <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
+              alt="logo"
+              layout="fill"
+              className="object-contain"
+            />
           </div>
-          <input
-            type="text"
-            placeholder="Search"
-            className="bg-gray-50 text-sm pl-10 border-gray-500 focus:ring-black focus:border-black rounded-md"
-          />
-        </div>
+          <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
+              alt="logo"
+              layout="fill"
+              className="object-contain"
+            />
+          </div>
 
-        {/* Right Side */}
+          <div className="relative">
+            <div className="absolute top-2 left-2">
+              <MagnifyingGlassIcon className="h-5" />
+            </div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-gray-50 text-sm pl-10 border-gray-500 focus:ring-black focus:border-black rounded-md"
+            />
+          </div>
 
-        <div className="flex space-x-4 items-center">
-          <HomeIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out hidden md:inline-flex" />
-          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          {/* Right Side */}
 
-          <img
-            src="https://avatars.githubusercontent.com/u/17096384?v=4"
-            alt="user-image"
-            className="h-10 rounded-full cursor-pointer"
-          />
+          <div className="flex space-x-4 items-center">
+            <HomeIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out hidden md:inline-flex" />
+            <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+
+            <img
+              src="https://avatars.githubusercontent.com/u/17096384?v=4"
+              alt="user-image"
+              className="h-10 rounded-full cursor-pointer"
+            />
+          </div>
         </div>
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,109 @@
-import Header from "../components/Header"
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    
-    // Header 
-        <Header />
-    // Feed
-
-    // Modal popup 
-  )
+    <>
+      {/* // Header */}
+      <Header />
+      {/* // Feed */}
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adip
+        <br />
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in volupt
+      </p>
+      
+      {/* // Modal popup */}
+    </>
+  );
 }
