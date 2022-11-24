be2b90775835fee7e0c4f344208779f393e4d1d4
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-11-24 16:22:33 +0530

    make feed section responsive

--- components/Feed.js
+++ components/Feed.js
@@ -1,17 +1,23 @@
-import Stories from "./Stories"
-import Posts from "./Posts"
+import Stories from "./Stories";
+import Posts from "./Posts";
 
 function Feed() {
   return (
-    <div>
+    <main className="grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto">
+      <section className="md:col-span-2">
         {/* Stories */}
         <Stories />
 
         {/* Posts */}
         <Posts />
-
-    </div>
-  )
+      </section>
+      <section>
+        {/* Mini profile */}
+        
+        {/* Suggestions  */}
+        </section>
+    </main>
+  );
 }
 
-export default Feed
+export default Feed;
