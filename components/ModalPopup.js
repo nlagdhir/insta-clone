e38c5363d63e95ea185fee266e5d2ccafa05df03
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-11-25 18:14:46 +0530

    install and implement react-modal

--- components/ModalPopup.js
+++ components/ModalPopup.js
@@ -1,16 +1,26 @@
-import {useRecoilState} from 'recoil';
-import modalState from '../atom/modalAtom';
+import { useRecoilState } from "recoil";
+import modalState from "../atom/modalAtom";
+import Modal from "react-modal";
 
 function ModalPopup() {
-
-    const [open, setOpen] = useRecoilState(modalState);
+  const [open, setOpen] = useRecoilState(modalState);
 
   return (
     <div>
-        Modal Popup
-        {open ? <h1>Modal is open</h1> : ''} 
+      Modal Popup
+      {open && (
+        <Modal
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+          className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] border-2 rounded-md shadow-md bg-white"
+        >
+          <div className="flex flex-col items-center justify-center h-[100%] ">
+            <h1>Modal</h1>
+          </div>
+        </Modal>
+      )}
     </div>
-  )
+  );
 }
 
-export default ModalPopup
+export default ModalPopup;
